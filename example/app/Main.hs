59b6929bad8ffe033427a76a1ceba6fc491d4fdc
Paul Burns <paul@flipstone.com>
2026-08-18 20:17:56 -0400

    Format the repository with fourmolu
    Mechanical only, no functional changes. Verify by re-running
    ./scripts/format-repo rather than by reading the diff.

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -1,14 +1,14 @@
 module Main (main) where
 
-import Network.FTP.Client
-import qualified Network.FTP.Client.Conduit as FC
 import Conduit
 import qualified Data.ByteString.Char8 as C
+import Network.FTP.Client
+import qualified Network.FTP.Client.Conduit as FC
 
 main :: IO ()
 main = withFTPS "hostname.com" 21 $ \h _welcome -> do
-    _ <- login h "username" "password"
-    runConduitRes
-        $ FC.mlsd h "."
-        .| mapC (C.pack . (<> "\n") . show)
-        .| stdoutC
+  _ <- login h "username" "password"
+  runConduitRes $
+    FC.mlsd h "."
+      .| mapC (C.pack . (<> "\n") . show)
+      .| stdoutC
